3cc876cfa78ff43dc39f685bd4ebf4c3749e8a37
ARBUCHELI <radio_seattle@hotmail.com>
2024-02-26 16:17:46 +0300

    #stye-methods - css & jquery

---
+++ css-and-jquery/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(() => {
+ 
+    $('.login-button').on('click', () => {
+      $('.login-form').toggle();
+    });
+    
+    $('.menu-button').on('mouseenter', () => {
+      $('.nav-menu').show();
+    })
+    
+    $('.nav-menu').on('mouseleave', () => {
+      $('.nav-menu').hide();
+    })
+    
+  }); 
+  
